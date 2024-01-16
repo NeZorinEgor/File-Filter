cb16033f63fbc639a355cd2a837aed03d2ab7e55
Zorin Egor <zorin-ep@mail.ru>
2024-01-16 18:46:48 +0800

    feat: add file writer

--- src/main/java/ru/neZorinEgor/task/Application.java
+++ src/main/java/ru/neZorinEgor/task/Application.java
@@ -1,35 +1,52 @@
 package ru.neZorinEgor.task;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Application {
-    //регулярные выражения для строки файла
+    //регулярные выражения
     static String isInt = "^[+-]?\\d+$"; //простые числа
     static String isString = "^(?![+-]?\\d)(?!\\n)[^\\d ].*$"; //слова
     static String isFloat = "[+-]?\\d+\\.\\d+([eE][+-]?\\d+)?"; //числа с плаваюзей запятой
 
-    public static void main(String[] args) throws FileNotFoundException {
+    static int countInt = 0;
+    static int cointString = 0;
+    static int cointFloat = 0;
+
+    public static void main(String[] args) throws IOException {
         var file = new File("src/main/java/ru/neZorinEgor/task/data/input/input.txt");
         int countLines = 0;
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()){
-            System.out.println(checkLineType(scanner.nextLine()));
+            checkLineType(scanner.nextLine());
             countLines++;
         }
+        System.out.println("int: " + countInt);
+        System.out.println("string: " + cointString);
+        System.out.println("float: " + cointFloat);
         scanner.close();
+
+
     }
-    public static String checkLineType(String line){
+    public static void checkLineType(String line) throws IOException {
+        FileWriter intWriter = new FileWriter("src/main/java/ru/neZorinEgor/task/data/output/integers.txt", true);
+        FileWriter stringWriter = new FileWriter("src/main/java/ru/neZorinEgor/task/data/output/strings.txt", true);
+        FileWriter floatWriter = new FileWriter("src/main/java/ru/neZorinEgor/task/data/output/floats.txt", true);
+
         if(line.matches(isInt)) {
-            return "int: " + line;
+            countInt++;
+            intWriter.append(line).append('\n');
+            intWriter.close();
         }
         if(line.matches(isString)) {
-            return "string: " + line;
+            cointString++;
+            stringWriter.append(line).append('\n');
+            stringWriter.close();
         }
         if (line.matches(isFloat)) {
-            return "float: " + line;
+            cointFloat++;
+            floatWriter.append(line).append('\n');
+            floatWriter.close();
         }
-        return "?: "+ line;
     }
 }
