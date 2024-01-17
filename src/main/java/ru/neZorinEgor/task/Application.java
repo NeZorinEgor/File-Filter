64eb9ab1afe33ac5ab5da3a8cb0d611f0b0571fd
Zorin Egor <zorin-ep@mail.ru>
2024-01-17 12:44:33 +0800

    feat: add switchFileWriter

--- src/main/java/ru/neZorinEgor/task/Application.java
+++ src/main/java/ru/neZorinEgor/task/Application.java
@@ -4,49 +4,68 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Application {
-    //регулярные выражения
-    static String isInt = "^[+-]?\\d+$"; //простые числа
-    static String isString = "^(?![+-]?\\d)(?!\\n)[^\\d ].*$"; //слова
-    static String isFloat = "[+-]?\\d+\\.\\d+([eE][+-]?\\d+)?"; //числа с плаваюзей запятой
-
+    //type counter
     static int countInt = 0;
     static int cointString = 0;
     static int cointFloat = 0;
 
+    //regex
+    static String isInt = "^[+-]?\\d+$"; //ints
+    static String isString = "^(?![+-]?\\d)(?!\\n)[^\\d ].*$"; //stings
+    static String isFloat = "[+-]?\\d+\\.\\d+([eE][+-]?\\d+)?"; //flots
+
+    static PrintWriter intWriter;
+    static PrintWriter stringWriter;
+    static PrintWriter floatWriter;
+
+    static File integers = new File("integers.txt");
+    static File strings = new File("strings.txt");
+    static File floats = new File("floats.txt");
+
     public static void main(String[] args) throws IOException {
-        var file = new File("src/main/java/ru/neZorinEgor/task/data/input/input.txt");
-        int countLines = 0;
-        Scanner scanner = new Scanner(file);
+        System.out.println("enter file path: ");
+        Scanner fileName = new Scanner(System.in);
+        File fileNameFromScanner = new File(fileName.nextLine());
+
+        Scanner scanner = new Scanner(fileNameFromScanner);
+
+        switchFileWriter(true);
+
         while (scanner.hasNextLine()){
             checkLineType(scanner.nextLine());
-            countLines++;
         }
+
         System.out.println("int: " + countInt);
         System.out.println("string: " + cointString);
         System.out.println("float: " + cointFloat);
         scanner.close();
 
-
     }
-    public static void checkLineType(String line) throws IOException {
-        FileWriter intWriter = new FileWriter("src/main/java/ru/neZorinEgor/task/data/output/integers.txt", true);
-        FileWriter stringWriter = new FileWriter("src/main/java/ru/neZorinEgor/task/data/output/strings.txt", true);
-        FileWriter floatWriter = new FileWriter("src/main/java/ru/neZorinEgor/task/data/output/floats.txt", true);
-
-        if(line.matches(isInt)) {
+    public static void checkLineType(String line){
+        if (line.matches(isInt)) {
             countInt++;
-            intWriter.append(line).append('\n');
-            intWriter.close();
+            intWriter.append(line).append(String.valueOf('\n'));
+            intWriter.flush();
         }
-        if(line.matches(isString)) {
+        if (line.matches(isString)) {
             cointString++;
-            stringWriter.append(line).append('\n');
-            stringWriter.close();
+            stringWriter.append(line).append(String.valueOf('\n'));
+            stringWriter.flush();
         }
         if (line.matches(isFloat)) {
             cointFloat++;
-            floatWriter.append(line).append('\n');
-            floatWriter.close();
+            floatWriter.append(line).append(String.valueOf('\n'));
+            floatWriter.flush();
+        }
+    }
+
+    public static void switchFileWriter(boolean action){
+        try {
+            intWriter = new PrintWriter(new FileWriter(integers, action));
+            stringWriter = new PrintWriter(new FileWriter(strings, action));
+            floatWriter = new PrintWriter(new FileWriter(floats, action));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
