d340217e8c8c0e7dac91c924348d2630c919c0d8
Zorin Egor <zorin-ep@mail.ru>
2024-01-22 23:36:08 +0800

    add test jar

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,36 +1,15 @@
 import ru.neZorinEgor.task.OptionManager.OptionManager;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(File.separator);
-//        OptionManager optionManager = new OptionManager();
-//        optionManager.getUserInput();
-//
-//        System.out.println("     path: " + optionManager.getPath());
-//        System.out.println("   prefix: " + optionManager.getPrefix());
-//        System.out.println("   append: " + optionManager.isAppendMode());
-//        System.out.println("full stat: " + optionManager.isFullStatistic());
-//        System.out.println("    files: " + optionManager.getFiles());
-//    }
-//
-//    public static String createFileDirectory(String path, String prefix, String fileName){
-//        if (prefix.isEmpty()){
-//            prefix ="";
-//        }
-//
-//        if (!path.isEmpty()){
-//            path += "/";
-//        }
-//
-//        if (path.isEmpty()){
-//            path = "";
-//        }
-//
-//        String fileFullName = prefix + fileName;
-//        String pathName = path;
-//
-//        return pathName + fileFullName;
+        String[] comand = new String[args.length];
+        for (int i = 0; i < args.length; i++) {
+            comand[i] = args[i];
+        }
+
+        System.out.println(Arrays.toString(comand));
     }
 }
